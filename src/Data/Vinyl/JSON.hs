5af8c3d5db67812697596b58012001f143d4b7db
Theo Belaire <theobelaire@khanacademy.org>
2014-03-26 01:56:16 -0700

    Now with JSON output
    Depends on vinyl 0.3,
    I'm going to match their version for now

--- src/Data/Vinyl/JSON.hs
+++ src/Data/Vinyl/JSON.hs
@@ -5,6 +5,9 @@
    , ScopedTypeVariables
    , KindSignatures
    , FlexibleContexts
+   , RankNTypes
+   , UndecidableInstances
+   , GADTs
    #-}
 {-| This adds and instance of FromJSON to SimpleRecords
  -
@@ -15,11 +18,12 @@ import Control.Applicative
 import Control.Monad
 
 import Data.Vinyl
-
+import Data.Vinyl.Idiom.Identity
 -- import Data.ByteString as L
 import Data.Aeson
+import Data.Aeson.Types
 
-import Data.Text as T
+import qualified Data.Text as T
 
 import GHC.TypeLits
 -- import Data.Proxy
@@ -42,5 +46,19 @@ instance (KnownSymbol sym, FromJSON a, FromJSON (PlainRec fields)) =>
               -- as it uses an internal type
               -- rest_rec :: Data.Aeson.Types.Internal.Parser (PlainRec (fields))
               -- but I needed to type rest_rec's result record to have it parse
+
+class HasPairs a where
+  toPairs :: a -> [Pair]
+
+instance HasPairs (PlainRec '[]) where
+  toPairs _ = []
+
+instance forall t rs s. (ToJSON t, HasPairs (PlainRec rs), KnownSymbol s)
+  => HasPairs (PlainRec (s:::t ': rs)) where
+  toPairs (Identity x :& xs) = 
+    (T.pack (show (Field :: s:::t)), toJSON x) : toPairs xs
+
+instance HasPairs (PlainRec rs) => ToJSON (PlainRec rs) where
+  toJSON = object . toPairs
 
 
