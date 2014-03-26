7f60ca4624f72775fe588083ac3127c9620e4bc3
Theo Belaire <theobelaire@khanacademy.org>
2014-03-26 01:54:33 -0700

    More example

--- src/Data/Vinyl/JSON/Examples.hs
+++ src/Data/Vinyl/JSON/Examples.hs
@@ -29,6 +29,9 @@ example_json = "{\"name\": \"jon\", \"job\":\"Code\", \"age\":42, \"things\":[1,
 
 nested_json = "{\"dad\" : { \"name\" : \"bob\" } }"
 
+
+name = Field :: ("name" ::: Text)
+
 parsed_example :: Maybe (PlainRec '[("name" ::: Text),
                                     ("job"  ::: Text),
                                     ("age"  ::: Int),
@@ -39,3 +42,11 @@ parsed_nested :: Maybe (PlainRec '[("dad" :::
                             (PlainRec '[("name" ::: Text)]))])
 parsed_nested = decode nested_json
 --}
+
+
+talk_about = case parsed_example of
+                    Nothing -> putStrLn "Failed to parse"
+                    (Just person) -> 
+                        putStrLn ("He is named: " 
+                                  ++ (T.unpack (rGet name person)))
+
